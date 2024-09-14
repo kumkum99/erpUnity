ff5fee83cfb500db585818b53b396f8a440fbb70
kumkum99 <kumkumkumari8899@gmail.com>
2024-09-14 12:32:23 +0530

    all working

--- src/main/java/com/springboot/web/controller/ForgotPasswordController.java
+++ src/main/java/com/springboot/web/controller/ForgotPasswordController.java
@@ -1,12 +1,17 @@
 package com.springboot.web.controller;
 
-import com.springboot.web.service.PasswordResetService;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.springboot.web.service.PasswordResetService;
 
 @RestController
 @RequestMapping("/api")
